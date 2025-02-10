fad5440f12038cf8162e0ab3da2a59f0bc8c5e99
Bell006 <bellamancio006@gmail.com>
2025-02-10 16:13:40 -0300

    Refactor: UI changes and bootstrap adapt

--- react.js/src/App.jsx
+++ react.js/src/App.jsx
@@ -1,188 +1,279 @@
-import React, { useState } from 'react';
-import InputMask from "react-input-mask";
-import { FaDownload } from "react-icons/fa6";
-import './App.css';
-import { api } from './services/api';
-import logo from './assets/Cidade-Buriti_logo.png';
+  import React, { useState } from 'react';
+  import InputMask from "react-input-mask";
+  import { FaDownload } from "react-icons/fa6";
+  import { api } from './services/api';
+  import logo from './assets/Cidade-Buriti_logo.png';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import './App.css';
+  import { Modal, Button } from 'react-bootstrap';
+  import loadingIcon from './assets/loadingAn.svg';
 
-function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    phone: '',
-    creci: '',
-    categories: []
-  });
-  
-  const [generatedImages, setGeneratedImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  
-  const categories = [
-    { value: 'investidor', label: 'Investidor' },
-    { value: 'localizacao', label: 'Localização' },
-    { value: 'petplace', label: 'Pet Place' },
-    { value: 'poliesportiva', label: 'Quadra poliesportiva' },
-    { value: 'condicoes1', label: 'Condições - 1' },
-    { value: 'condicoes2', label: 'Condições - 2' },
-    { value: 'general', label: 'Geral' },
-    { value: 'playground', label: 'Playground' },
-    { value: 'quadraAreia', label: 'Quadra de areia' },
-    { value: 'areaLazer', label: 'Área de lazer' },
-  ];
-  
-  let digit = /[0-9]/;
-  let mobileMask = ['(', digit, digit, ')', ' ', '9', ' ', digit, digit, digit, digit, '-', digit, digit, digit, digit];
-  
-  const handleApiError = (error) => {
-    console.log('Error details:', error);
+  function App() {
+    const [formData, setFormData] = useState({
+      name: '',
+      phone: '',
+      creci: '',
+      categories: []
+    });
 
-    if (error.response && error.response.data && error.response.data.message) {
-      alert(error.response.data.message);
-    } else if (error.request) {
-      alert('A requisição foi feita, mas não houve resposta. Verifique a configuração do servidor.');
-    } else {
-      alert('Erro ao criar as imagens. Por favor, tente novamente mais tarde.');
-    }
-  };
-  
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    
-    if (type === 'checkbox') {
-      const updatedCategories = checked
-      ? formData.categories.includes(value) || formData.categories.length >= 5
-      ? formData.categories
-      : [...formData.categories, value]
-      : formData.categories.filter((category) => category !== value);
-      
-      setFormData({
-        ...formData,
-        categories: updatedCategories,
-      });
-      
-      if (checked && formData.categories.length >= 5) {
-        alert('Selecione no máximo 5 categorias por vez.');
-      }
-    } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    }
-  };
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    
-    try {
-      setGeneratedImages([]);
+    const [generatedImages, setGeneratedImages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-      const response = await api.post('/create_image', formData, {
-        headers: {
-          'Content-Type': 'application/json'
+    const categories = [
+      { value: 'investidor', label: 'Investidor' },
+      { value: 'localizacao', label: 'Localização' },
+      { value: 'petplace', label: 'Pet Place' },
+      { value: 'poliesportiva', label: 'Quadra poliesportiva' },
+      { value: 'condicoes1', label: 'Condições - 1' },
+      { value: 'condicoes2', label: 'Condições - 2' },
+      { value: 'general', label: 'Geral' },
+      { value: 'playground', label: 'Playground' },
+      { value: 'quadraAreia', label: 'Quadra de areia' },
+      { value: 'areaLazer', label: 'Área de lazer' },
+    ];
+
+    let digit = /[0-9]/;
+    let mobileMask = ['(', digit, digit, ')', ' ', '9', ' ', digit, digit, digit, digit, '-', digit, digit, digit, digit];
+
+    const handleApiError = (error) => {
+      console.log('Error details:', error);
+      alert(error.response?.data?.message || 'Erro ao criar as imagens.');
+    };
+
+    const handleChange = (e) => {
+      const { name, value, type, checked } = e.target;
+
+      if (type === 'checkbox') {
+        const updatedCategories = checked
+          ? formData.categories.includes(value) || formData.categories.length >= 5
+            ? formData.categories
+            : [...formData.categories, value]
+          : formData.categories.filter((category) => category !== value);
+
+        setFormData({
+          ...formData,
+          categories: updatedCategories,
+        });
+
+        if (checked && formData.categories.length >= 5) {
+          alert('Selecione no máximo 5 categorias por vez.');
         }
-      });
-
-      if (response.status === 200) {
-        setGeneratedImages(response.data.generated_images || []);
       } else {
-        alert(`${response.data.message}`);
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
       }
-    } catch (error) {
-      handleApiError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    };
 
-  return (
-    <div className="App">
-      <div className="app-wrapper">
-        <header>
-          <img src={logo} alt="Logo" width="120"/>
-          <div className="headerWrapper">
-            <h1>Gerador de peças</h1>
-            <p>Corretores</p>
-          </div>
-        </header>
-        <form onSubmit={handleSubmit}>
-          <div className="inputWrapper">
-            <label>
-              Nome:
-            </label>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Maria da Silva' required />
-          </div>
+    const handleSubmit = async (e) => {
+      e.preventDefault();
 
-          <div className="inputWrapper">
-            <label>
-              Celular:
-            </label>
-              <InputMask type='text' name='phone' value={formData.phone} onChange={handleChange} mask={mobileMask} placeholder='(64) 9 5647-7582' required/>
-          </div>
+      if (formData.categories.length === 0) {
+        alert('Selecione ao menos uma categoria.');
+        return;
+      }
 
-          <div className="inputWrapper">
-            <label>
-              CRECI:
-            </label>
-            <input type="number" name="creci" value={formData.creci} onChange={handleChange} placeholder='45287' required />
-          </div>
+      setShowModal(true);
+    };
 
-          <div className="inputWrapper categories">
-            <label className='categoriesTitle'>Categorias:</label>
-            <div className="checkboxGroup">
-              {categories.map(category => (
-                <label key={category.value}>
-                  <input
-                    type="checkbox"
-                    value={category.value}
-                    checked={formData.categories.includes(category.value)}
-                    onChange={handleChange}
-                  />
-                  {category.label}
-                </label>
-              ))}
-            </div>
-          </div>
+    const handleConfirmSubmit = async (e) => {
+      setShowModal(false);
+      setLoading(true);
 
-          <button type="submit" className="createButton" disabled={loading}>
-            {loading ? 'Aguarde...' : 'Gerar peças'}
-          </button>
-        </form>
-        
-          {generatedImages.length > 0 && (
-            <div className="resultsContainer">
-              <h2>Imagens Geradas</h2>
-              <div className="imagesContainer">
-                {generatedImages.map((image, index) => (
-                  <div key={index} className="imageCard">
-                    <p>{image.category}</p>
-                    <div className="imageLinks">
-                      <a
-                        href={image.feed_image_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="downloadButton"
-                      >
-                        <FaDownload /> Feed 
-                      </a>
-                      <a
-                        href={image.stories_image_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="downloadButton"
-                      >
-                        <FaDownload /> Stories
-                      </a>
+      try {
+        setGeneratedImages([]);
+        const response = await api.post('/create_image', formData, {
+          headers: { 'Content-Type': 'application/json' }
+        });
+
+        setGeneratedImages(response.data.generated_images || []);
+      } catch (error) {
+        handleApiError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const handleCloseModal = () => {
+      setShowModal(false);
+    };
+
+    return (
+      <div className="container-fluid p-4">
+        <div className="row">
+          {/* Left Column (Header and Images) */}
+          <div className="col-12 col-lg-6">
+            <div className="d-lg-flex flex-column justify-content-between" style={{height: '100%'}}>
+
+              {/* Header */}
+              <div className="header-container">
+                <div className="d-flex align-items-center">
+                  <img src={logo} alt="Logo" width="140" className="me-3"/>
+                  <div>
+                    <h1 className="h2 mb-0" style={{color: '#ffffff'}}>Gerador de peças</h1>
+                    <p className="mb-0" style={{color: '#cef146'}}>Corretores</p>
+                  </div>
+                </div>
+
+                <div>
+                  <label className="subdivision-label mb-2 mt-4 ">Empreendimento:</label>
+                  <select class="form-select" aria-label="Default select example">
+                      <option selected>Cidade Buriti</option>
+                      <option value="1">Cidade Buriti</option>
+                      <option value="2">Cidade Buriti</option>
+                      <option value="3">Cidade Buriti</option>
+                  </select>
+                </div>
+              </div>
+              
+              {/* Images */}
+              <div className="images-container">
+                  <div className="results-container">
+                    <div className="results-card d-flex align-items-center mt-4 p-3">
+                          {generatedImages.length <= 0 && (
+                            <p className="text-center w-100 m-0">
+                              {loading ? <img src={loadingIcon}/> : 'Nenhuma peça gerada ainda.'}
+                            </p>
+                          )}
+                    <div className="card-body card-body-scrollable">
+                        <div className="row">
+                          {generatedImages.map((image, index) => (
+                            <div key={index} className="col-12 col-md-4 mb-3">
+                              <div className="card card-animated">
+                                <div className="card-header">{image.category}</div>
+                                <div className="card-body px-2 py-2">
+                                  <a
+                                    href={image.feed_image_url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="btn btn-outline-custom d-flex align-items-center mb-2"
+                                  >
+                                    <FaDownload className="me-2" /> Feed
+                                  </a>
+                                  <a
+                                    href={image.stories_image_url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="btn btn-outline-custom d-flex align-items-center"
+                                  >
+                                    <FaDownload className="me-2" /> Stories
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                ))}
               </div>
             </div>
-          )}
+          </div>
+
+          {/* Right Column (Form) */}
+          <div className="col-12 col-lg-6">
+            <div className="card shadow mt-4">
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3">
+                    <label className="form-label">Nome:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      placeholder='Maria da Silva'
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">Celular:</label>
+                    <InputMask
+                      type='text'
+                      className="form-control"
+                      name='phone'
+                      value={formData.phone}
+                      onChange={handleChange}
+                      mask={mobileMask}
+                      placeholder='(64) 9 5647-7582'
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">CRECI:</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="creci"
+                      value={formData.creci}
+                      onChange={handleChange}
+                      placeholder='45287'
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">Categorias:</label>
+                    <div className="row">
+                      {categories.map(category => (
+                        <div key={category.value} className="col-6 col-md-4 mb-2">
+                          <div className="form-check">
+                            <input
+                              type="checkbox"
+                              className="form-check-input"
+                              id={`category-${category.value}`}
+                              value={category.value}
+                              checked={formData.categories.includes(category.value)}
+                              onChange={handleChange}
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor={`category-${category.value}`}
+                            >
+                              {category.label}
+                            </label>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    disabled={loading}
+                  >
+                    {loading ? 'Aguarde...' : <span className="fw-bold">Gerar peças</span>}
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <Modal show={showModal} onHide={handleCloseModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirmação</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Ao clicar em 'Entendi!', você concorda com o armazenamento das informações fornecidas para melhorias de navegação e análise de uso do site.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={handleCloseModal}>
+              Cancelar
+            </Button>
+            <Button variant="primary" onClick={handleConfirmSubmit}>
+              Entendi!
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-  );
-}
+    );
+  }
 
-export default App;
-
+  export default App;
