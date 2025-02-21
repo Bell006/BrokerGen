29da877aa1077a0a56f66c7dedbceb18a5a1af3a
Bell006 <bellamancio006@gmail.com>
2025-02-21 14:01:11 -0300

    Feature: implement CSRF token in API configs

--- react.js/src/services/api.js
+++ react.js/src/services/api.js
@@ -1,5 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: "https://brokergen.onrender.com"
-})
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    withCredentials: true,
+    xsrfCookieName: 'csrf_token',
+    xsrfHeaderName: "x-csrftoken",
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+});
+
+api.interceptors.request.use(config => {
+    if (['post', 'put', 'delete', 'get'].includes(config.method.toLowerCase())) {
+        const csrfToken = document.cookie
+            .split('; ')
+            .find(row => row.startsWith('csrf_token='))
+            ?.split('=')[1];
+
+        if (csrfToken) {
+            config.headers['x-csrftoken'] = csrfToken;
+        }
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+export { api };
