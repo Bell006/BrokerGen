ca03fb805066b30ee5fde7aebb882fed86c09231
Bell Amancio de Melo Costa <bellamancio006@gmail.com>
2025-04-17 15:12:24 -0300

    Create constants folder

---
+++ react.js/src/constants/categoriesAndEnterprises.js
@@ -0,0 +1,27 @@
+export const data = {
+    categories: [
+      { value: 'investidor', label: 'Investidor' },
+      { value: 'localizacao', label: 'Localização' },
+      { value: 'petplace', label: 'Pet Place' },
+      { value: 'poliesportiva', label: 'Quadra poliesportiva' },
+      { value: 'condicoes1', label: 'Condições - 1' },
+      { value: 'condicoes2', label: 'Condições - 2' },
+      { value: 'general', label: 'Geral' },
+      { value: 'playground', label: 'Playground' },
+      { value: 'quadraAreia', label: 'Quadra de areia' },
+      { value: 'areaLazer', label: 'Área de lazer' },
+    ],
+    
+    enterprises: {
+      'Palmas/TO': [
+        { id: 1, name: 'Buriti Garden Premium' },
+        { id: 2, name: 'Cidade Buriti' },
+      ],
+      'Redenção/PA': [
+        { id: 3, name: 'Buriti Garden Premium' },
+      ],
+      'Barreiras/BA': [
+        { id: 4, name: 'Jardim Ipiranga' },
+      ],
+    },
+};
