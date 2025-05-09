81779ddda6cd3dd2afeedd2c40f88de78b61c3c0
Bell Amancio de Melo Costa <bellamancio006@gmail.com>
2025-05-09 15:45:47 -0300

    Updade with new categories

--- react.js/src/constants/categoriesAndEnterprises.js
+++ react.js/src/constants/categoriesAndEnterprises.js
@@ -4,23 +4,31 @@ export const data = {
       { id: 1, name: 'Buriti Garden Premium', categories: ['general'] },
     ],
     'Redenção/PA': [
-      { id: 3, name: 'Buriti Garden Premium', categories: ['general'] },
+      { id: 3, name: 'Buriti Garden Premium', categories: ['general', 'lazer_esp', 'lazer_saude', 'lazer_clube', 'lazer', 'seguranca', 'lago'] },
     ],
     'Barreiras/BA': [
-      { id: 4, name: 'Jardim Ipiranga', categories: ['general'] },
+      { id: 4, name: 'Jardim Ipiranga', categories: ['general', 'investidor_a', 'investidor_b', 'localizacao', 'condicoes', 'infra', 'quadraAreia', 'lazer', 'lazer_b'] },
+
     ],
   },
 
   categories: [
-    { value: 'investidor', label: 'Investidor' },
+    { value: 'investidor_a', label: 'Investidor - 1' },
+    { value: 'investidor_b', label: 'Investidor - 2' },
     { value: 'localizacao', label: 'Localização' },
     { value: 'petplace', label: 'Pet Place' },
     { value: 'poliesportiva', label: 'Quadra poliesportiva' },
-    { value: 'condicoes1', label: 'Condições - 1' },
+    { value: 'condicoes', label: 'Condições' },
     { value: 'condicoes2', label: 'Condições - 2' },
     { value: 'general', label: 'Geral' },
-    { value: 'playground', label: 'Playground' },
+    { value: 'infra', label: 'Infraestrutura' },
     { value: 'quadraAreia', label: 'Quadra de areia' },
-    { value: 'areaLazer', label: 'Área de lazer' },
+    { value: 'lazer', label: 'Área de lazer - 1' },
+    { value: 'lazer_b', label: 'Área de lazer - 2' },
+    { value: 'lazer_esp', label: 'Área de lazer - Esporte' },
+    { value: 'lazer_saude', label: 'Área de lazer - Saúde' },
+    { value: 'lazer_clube', label: 'Área de lazer - Clube' },
+    { value: 'seguranca', label: 'Segurança' },
+    { value: 'lago', label: 'Lago' },
   ],
 };
